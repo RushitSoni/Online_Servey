c018a63bc6bdf93908c52e72b153156ee21b797a
RushitSoni <115700646+RushitSoni@users.noreply.github.com>
2024-01-04 01:48:24 +0530

    02.Login,Register,Refresh-user-token

---
+++ Api/DTOs/Account/RegisterDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.DTOs.Account
+{
+    public class RegisterDto
+    {
+        [Required]
+        [StringLength(15 , MinimumLength =3,ErrorMessage ="First Name must be atleast {2} , and Maximum {1}")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "Last Name must be atleast {2} , and Maximum {1}")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 6, ErrorMessage = "PassWord must be atleast {2} , and Maximum {1}")]
+        public string Password { get; set; }
+
+        [Required]
+//[RegularExpression("", ErrorMessage ="Invalid Email Address")]
+        public string Email { get; set; }
+
+    }
+}
